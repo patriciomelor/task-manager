9df3fe0c7ec78810f5af0cfc33ef792bc2782368
Patricio Melo <42676797+patriciomelor@users.noreply.github.com>
2025-09-30 00:45:08 -0300

    Agrega backend/users y backend/tasks como carpetas normales

---
+++ backend/users/src/app.module.ts
@@ -0,0 +1,26 @@
+// backend/users/src/app.module.ts (y tasks)
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(), // Variable que usaremos
+        PORT: Joi.number().default(3000),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URI,
+      }),
+    }),
+    // ... aquí van tus módulos de Features (UsersModule, TasksModule)
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
